7825feaadfb2c566293f500efc534a148f1d8d3e
Dell <Dell@DESKTOP-GIG0893>
2024-03-28 23:37:26 +0530

    springboot1

---
+++ src/main/java/com/kodnest/springboot1/controller/Demo.java
@@ -0,0 +1,15 @@
+package com.kodnest.springboot1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+	
+	@GetMapping(value="/greet")
+	public String greet()
+	{
+		return "welcome to spring-boot Rakshaaaa";
+	}
+
+}
